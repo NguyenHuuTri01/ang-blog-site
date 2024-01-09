a2ffa042365f265bd6e6a1755fc723c1fee2d3e2
Nguyen Huu Tri <nguyenhuutri0905@gmail.com>
2024-01-09 09:49:31 +0700

    logout component

--- ang-blog-dashboard/src/app/layouts/header/header.component.ts
+++ ang-blog-dashboard/src/app/layouts/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -8,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   userData: any = localStorage.getItem('user');
   userEmail: string = '';
+  isLoggedIn$: Observable<boolean> = this.authService.isLoggedIn();
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
-    this.userEmail = JSON.parse(this.userData).email
+    this.userEmail = JSON.parse(this.userData).email;
+  }
+  onLogOut() {
+    this.authService.logOut();
   }
 
 }
--- ang-blog-dashboard/src/app/services/auth.service.ts
+++ ang-blog-dashboard/src/app/services/auth.service.ts
@@ -2,11 +2,14 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  loggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   constructor(
     private afAuth: AngularFireAuth,
@@ -18,6 +21,7 @@ export class AuthService {
     return this.afAuth.signInWithEmailAndPassword(email, password).then(logRef => {
       this.toastr.success('Logged In Successfully');
       this.loadUser();
+      this.loggedIn.next(true);
       this.router.navigate(['/']);
     }).catch(e => {
       this.toastr.warning(e.message);
@@ -28,6 +32,17 @@ export class AuthService {
     this.afAuth.authState.subscribe(user => {
       localStorage.setItem('user', JSON.stringify(user));
     })
+  }
+  logOut() {
+    this.afAuth.signOut().then(() => {
+      this.toastr.success('User Logged Out Successfuly');
+      localStorage.removeItem('user');
+      this.loggedIn.next(false);
+      this.router.navigate(['/login']);
+    });
+  }
+  isLoggedIn() {
+    return this.loggedIn.asObservable();
   }
 
 }
