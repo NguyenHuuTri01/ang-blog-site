78c5bb7d246d817a8333161a5e4879f28c93c8bc
Nguyen Huu Tri <nguyenhuutri0905@gmail.com>
2025-05-22 14:40:29 +0700

    hidden file evironment

---
+++ ang-blog-dashboard/src/environments/environment.development.example.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "YOUR_API_KEY_HERE",
+        authDomain: "YOUR_PROJECT.firebaseapp.com",
+        projectId: "YOUR_PROJECT_ID",
+        storageBucket: "YOUR_PROJECT.appspot.com",
+        messagingSenderId: "YOUR_SENDER_ID",
+        appId: "YOUR_APP_ID"
+    }
+};
